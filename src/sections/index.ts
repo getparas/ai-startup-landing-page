6f2096c956be61a299f1f01a291a33b89d754a18
getparas <karkiparas020@gmail.com>
2025-09-15 22:19:52 +0545

    feat(sections): add barrel exports for sections components

---
+++ src/sections/index.ts
@@ -0,0 +1,7 @@
+export { Header } from "./Header";
+export { Hero } from "./Hero";
+export { Testimonials } from "./Testimonials";
+export { CallToAction } from "./CallToAction";
+export { Footer } from "./Footer";
+export { Features } from "./Features";
+export { LogoTicker } from "./LogoTicker";
