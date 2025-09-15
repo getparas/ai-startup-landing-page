835a255289bf661bd758777097269b821b0afb28
getparas <karkiparas020@gmail.com>
2025-09-15 22:20:38 +0545

    feat(assets): add barrel exports for assets images, icons

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+// PNG
+export { default as Avatar1 } from "./avatar-1.png";
+export { default as Avatar2 } from "./avatar-2.png";
+export { default as Avatar3 } from "./avatar-3.png";
+export { default as Avatar4 } from "./avatar-4.png";
+export { default as LogoAcme } from "./logo-acme.png";
+export { default as LogoApex } from "./logo-apex.png";
+export { default as LogoCelestial } from "./logo-celestial.png";
+export { default as LogoEcho } from "./logo-echo.png";
+export { default as LogoPulse } from "./logo-pulse.png";
+export { default as LogoQuantum } from "./logo-quantum.png";
+export { default as GridLines } from "./grid-lines.png";
+export { default as ProductImage } from "./product-image.png";
+export { default as Stars } from "./stars.png";
+// SVG
+export { default as LogoIcon } from "./logo.svg";
+export { default as IconClick } from "./icon-click.svg";
+export { default as IconGuage } from "./icon-guage.svg";
+export { default as IconMenu } from "./icon-menu.svg";
+export { default as IconStars } from "./icon-stars.svg";
+export { default as Instagram } from "./social-instagram.svg";
+export { default as X } from "./social-x.svg";
+export { default as Youtube } from "./social-youtube.svg";
