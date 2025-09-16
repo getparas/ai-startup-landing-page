dbe35e55e3544dab136336ad3ba1de2ddc8a4894
getparas <karkiparas020@gmail.com>
2025-09-16 11:47:05 +0545

    add prettier plugin for tailwind css that automatically sorts classes based on class order

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,33 +3,33 @@ import { Button } from "@/components";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
-      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
+    <header className="sticky top-0 z-10 border-b border-white/15 py-4 md:border-none">
+      <div className="absolute inset-0 -z-10 backdrop-blur md:hidden"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
-          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
+        <div className="relative mx-auto flex max-w-2xl items-center justify-between rounded-xl border-white/15 md:border md:p-2.5">
+          <div className="absolute inset-0 -z-10 hidden backdrop-blur md:block"></div>
           <div>
-            <div className="border size-10 rounded-lg inline-flex justify-center items-center border-white/15">
+            <div className="inline-flex size-10 items-center justify-center rounded-lg border border-white/15">
               <LogoIcon className="size-8" />
             </div>
           </div>
           <div className="hidden md:block">
             <nav className="flex gap-8 text-sm">
-              <a href="#" className="text-white/70 hover:text-white transition">
+              <a href="#" className="text-white/70 transition hover:text-white">
                 Features
               </a>
-              <a href="#" className="text-white/70 hover:text-white transition">
+              <a href="#" className="text-white/70 transition hover:text-white">
                 Developers
               </a>
-              <a href="#" className="text-white/70 hover:text-white transition">
+              <a href="#" className="text-white/70 transition hover:text-white">
                 Pricing
               </a>
-              <a href="#" className="text-white/70 hover:text-white transition">
+              <a href="#" className="text-white/70 transition hover:text-white">
                 Changelog
               </a>
             </nav>
           </div>
-          <div className="flex gap-4 items-center">
+          <div className="flex items-center gap-4">
             <Button>Join waitlist</Button>
             <IconMenu className="md:hidden" />
           </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,42 +4,42 @@ import { Button } from "@/components";
 export const Hero = () => {
   return (
     <section
-      className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
+      className="relative flex h-[492px] items-center overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)] md:h-[800px]"
       style={{
         backgroundImage: `url(${Stars.src})`,
       }}
     >
       <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
       {/* Start Planet */}
-      <div className="absolute size-64 md:size-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+      <div className="absolute left-1/2 top-1/2 size-64 -translate-x-1/2 -translate-y-1/2 rounded-full border border-white/20 bg-purple-500 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)] md:size-96"></div>
       {/* End Planet */}
       {/* Start Ring 1 */}
-      <div className="absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] border opacity-20 rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="absolute h-2 w-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-        <div className="absolute h-2 w-2 left-1/2 bg-white rounded-full top-0 -translate-x-1/2 -translate-y-1/2"></div>
-        <div className="absolute h-5 w-5 left-full border border-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center">
-          <div className="h-2 w-2 bg-white rounded-full"></div>
+      <div className="absolute left-1/2 top-1/2 h-[344px] w-[344px] -translate-x-1/2 -translate-y-1/2 rounded-full border opacity-20 md:h-[580px] md:w-[580px]">
+        <div className="absolute left-0 top-1/2 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
+        <div className="absolute left-1/2 top-0 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
+        <div className="absolute left-full top-1/2 inline-flex h-5 w-5 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full border border-white">
+          <div className="h-2 w-2 rounded-full bg-white"></div>
         </div>
       </div>
       {/* End Ring 1 */}
       {/* Start Ring 2 */}
-      <div className="absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] rounded-full border border-dashed border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute left-1/2 top-1/2 h-[444px] w-[444px] -translate-x-1/2 -translate-y-1/2 rounded-full border border-dashed border-white/20 md:h-[780px] md:w-[780px]"></div>
       {/* End Ring 2 */}
       {/* Start Ring 3 */}
-      <div className="absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full border border-white opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="absolute h-2 w-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-        <div className="absolute h-2 w-2 left-full bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute left-1/2 top-1/2 h-[544px] w-[544px] -translate-x-1/2 -translate-y-1/2 rounded-full border border-white opacity-20 md:h-[980px] md:w-[980px]">
+        <div className="absolute left-0 top-1/2 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
+        <div className="absolute left-full top-1/2 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
       </div>
       {/* End Ring 3 */}
       <div className="container relative mt-16">
-        <h1 className="text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+        <h1 className="bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] bg-clip-text text-center text-8xl font-semibold tracking-tighter text-transparent md:text-[168px] md:leading-none">
           AI SEO
         </h1>
-        <p className="text-lg md:text-xl text-white/70 mt-5 text-center max-w-xl mx-auto">
+        <p className="mx-auto mt-5 max-w-xl text-center text-lg text-white/70 md:text-xl">
           Elevate your site&apos;s visibility effortlessly with AI, where smart
           technology meets user-friendly SEO tools.
         </p>
-        <div className="flex justify-center mt-5">
+        <div className="mt-5 flex justify-center">
           <Button>Join waitlist</Button>
         </div>
       </div>
